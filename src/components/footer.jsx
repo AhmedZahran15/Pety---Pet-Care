35c6bce164597a83499e320946e06fe51b238bd8
DaliaZH <ahmedzhran876@gmail.com>
2024-01-21 04:40:39 +0200

    refactor footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,12 +9,18 @@ const FooterLink = ({ href, text }) => {
     </a>
   );
 };
-
+const SocialLink = ({ href, src, alt }) => {
+  return (
+    <a className="hover:scale-150 transition-all duration-200" href={href}>
+      <img src={src} alt={alt} />
+    </a>
+  );
+};
 const Footer = () => {
   return (
     <footer className="mx-10 my-8 flex flex-wrap items-start justify-start gap-y-4 pt-10 sm:justify-around">
       <div className="w-48">
-        <div className="mb-2 text-3xl font-bold">Pety</div>
+        <div className="mb-2 text-3xl font-bold">PETY</div>
         <FooterLink href="#" text="Terms of Service" />
       </div>
       <div className="flex w-40 flex-col gap-3">
@@ -33,30 +39,41 @@ const Footer = () => {
         <div className='font-["Product Sans"] flex w-48 flex-wrap text-3xl font-bold'>
           Stay in Touch
           <div className="my-5 flex flex-row  gap-2">
-            <a href="#">
-              <img src="/images/homepage/footer/Facebook.svg" alt="facebook" />
-            </a>
-            <a href="#">
-              <img src="/images/homepage/footer/Twitter.svg" alt="Twitter" />
-            </a>
-            <a href="#">
-              <img
-                src="/images/homepage/footer/Instagram.svg"
-                alt="Instagram"
-              />
-            </a>
-            <a href="#">
-              <img src="/images/homepage/footer/LinkedIn.svg" alt="LinkedIn" />
-            </a>
-            <a href="#">
-              <img src="/images/homepage/footer/Group.svg" alt="youtube" />
-            </a>
-            <a href="#">
-              <img src="/images/homepage/footer/Discord.svg" alt="discord" />
-            </a>
-            <a href="#">
-              <img src="/images/homepage/footer/whatsapp.svg" alt="whatsapp" />
-            </a>
+            <SocialLink
+              href="#"
+              src="/images/homepage/footer/Facebook.svg"
+              alt="facebook"
+            />
+            <SocialLink
+              href="#"
+              src="/images/homepage/footer/Twitter.svg"
+              alt="Twitter"
+            />
+            <SocialLink
+              href="#"
+              src="/images/homepage/footer/Instagram.svg"
+              alt="Instagram"
+            />
+            <SocialLink
+              href="#"
+              src="/images/homepage/footer/LinkedIn.svg"
+              alt="LinkedIn"
+            />
+            <SocialLink
+              href="#"
+              src="/images/homepage/footer/Group.svg"
+              alt="youtube"
+            />
+            <SocialLink
+              href="#"
+              src="/images/homepage/footer/Discord.svg"
+              alt="discord"
+            />
+            <SocialLink
+              href="#"
+              src="/images/homepage/footer/whatsapp.svg"
+              alt="whatsapp"
+            />
           </div>
         </div>
       </div>
