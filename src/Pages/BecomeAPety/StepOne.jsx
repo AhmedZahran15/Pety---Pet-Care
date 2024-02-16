5b4936993c7d80f9349feb486b34b385ae9f63b7
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-02-16 15:52:01 +0200

    Revert "Add files via upload"
    This reverts commit 004ae74c74ae3b816fbd9c405fa32564cfc9527a.

--- src/Pages/BecomeAPety/StepOne.jsx
+++ src/Pages/BecomeAPety/StepOne.jsx
@@ -1,52 +1,8 @@
-import Labels from "./Labels"
-import { Link } from "react-router-dom";
-import FormContext from "../../contexts/FormContext";
-import { useContext } from "react";
-import { validateName } from "../../utils/validationFunctions";
-
-
 function StepOne() {
-  const {Username,setUsername,ServiceName,setServiceName}=useContext(FormContext)
- 
     return (
-        <div className=" bg-neutral-100 flex   w-full   flex-col    items-center pt-40  ">
-
-<div className="h-1 w-2/5 bg-neutral-200 ">
-          <div className="h-1 bg-primary" style={{ width: "0%" }}></div>
-          <div className=" flex justify-around ">
-          <div className="text-black  font-normal  text-lg  font-[Montserrat] ">Name</div>
-          <div className="text-black  font-normal  text-lg font-[Montserrat] ">Service</div>
-          <div className="text-black  font-normal  text-lg  font-[Montserrat] ">
-
-Contact info</div>
-          </div>
-      </div>
-
-
-            <form className=" border-0 rounded-lg bg-white mt-14   w-3/6  pb-8  ">
-<Labels
-LabelText="Enter Your Name"
-Fieldinput={Username}
-SetFieldInput={setUsername}
-
-/>
-<Labels
-LabelText="Enter Your Service / Service Name"
-Fieldinput={ServiceName}
-SetFieldInput={setServiceName}
-/>
-<div className="mt-6 ml-6 flex items-center justify-start gap-x-6">
-<Link
-          className="rounded-md bg-[#ffa500] px-12 py-4 text-sm font-semibold text-white "
-          to={"/becomeAPety/steptwo"}  >
-          Next
-        </Link>
-
-
-      </div>
-      </form>
+        <div>
+            One
         </div>
-
     )
 }
 
--- src/Pages/BecomeAPety/index.jsx
+++ src/Pages/BecomeAPety/index.jsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
-import FormContext from "../../contexts/FormContext";
-import { FormProvider } from "../../contexts/FormContext";
+
 function BecomeAPety() {
   return (
-    <FormProvider>
     <div className="h-full w-full bg-neutral-100">
       <Outlet />
     </div>
-    </FormProvider>
   );
 }
 
