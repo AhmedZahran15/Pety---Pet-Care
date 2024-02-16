6837eaaa286f61ea897189541f830bc806dda19b
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-02-16 17:46:15 +0200

    Scroll to top on page load

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -20,7 +20,7 @@ function Services() {
     page: "1",
   });
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" })
+    window.scrollTo({ top: 200, behavior: "smooth" })
     const controller = new AbortController();
     async function fetchServices() {
       try {
