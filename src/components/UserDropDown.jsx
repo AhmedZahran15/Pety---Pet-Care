22d743a72106d5a03faa17a2c34700d3dd89388f
DaliaZH <ahmedzhran876@gmail.com>
2024-02-28 12:47:33 +0200

    Update UserDropDown component

--- src/components/UserDropDown.jsx
+++ src/components/UserDropDown.jsx
@@ -8,7 +8,7 @@ function UserDropDown() {
 
   useEffect(() => {
     const closeEvent = (e) => {
-      if (e.target.closest(".relative") !== null) return;
+      if (e.target.closest(".userDropDown") !== null) return;
       setIsOpen(false);
     };
     if (isOpen) {
@@ -19,7 +19,7 @@ function UserDropDown() {
     };
   }, [isOpen]);
   return (
-    <div className="relative inline-block text-left">
+    <div className="userDropDown relative inline-block text-left">
       <div>
         <button
           onClick={() => setIsOpen((prev) => !prev)}
@@ -57,7 +57,7 @@ function UserDropDown() {
           to="/dashboard"
           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
         >
-          DashBoard
+          Dashboard
         </Link>
         <Link
           to="/profile"
@@ -74,9 +74,42 @@ function UserDropDown() {
         <button
           onClick={() => logoutUser()}
           type="button"
-          className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+          className="flex w-full items-center justify-start gap-x-2 px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
         >
-          Sign out
+          <span className="h-3 w-3">
+            <svg
+              className="fill-gray-500"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 512 512"
+              xmlSpace="preserve"
+            >
+              <g>
+                <g>
+                  <path
+                    d="M255.15,468.625H63.787c-11.737,0-21.262-9.526-21.262-21.262V64.638c0-11.737,9.526-21.262,21.262-21.262H255.15
+			c11.758,0,21.262-9.504,21.262-21.262S266.908,0.85,255.15,0.85H63.787C28.619,0.85,0,29.47,0,64.638v382.724
+			c0,35.168,28.619,63.787,63.787,63.787H255.15c11.758,0,21.262-9.504,21.262-21.262
+			C276.412,478.129,266.908,468.625,255.15,468.625z"
+                  />
+                </g>
+              </g>
+              <g>
+                <g>
+                  <path
+                    d="M505.664,240.861L376.388,113.286c-8.335-8.25-21.815-8.143-30.065,0.213s-8.165,21.815,0.213,30.065l92.385,91.173
+			H191.362c-11.758,0-21.262,9.504-21.262,21.262c0,11.758,9.504,21.263,21.262,21.263h247.559l-92.385,91.173
+			c-8.377,8.25-8.441,21.709-0.213,30.065c4.167,4.21,9.653,6.336,15.139,6.336c5.401,0,10.801-2.041,14.926-6.124l129.276-127.575
+			c4.04-3.997,6.336-9.441,6.336-15.139C512,250.302,509.725,244.88,505.664,240.861z"
+                  />
+                </g>
+              </g>
+            </svg>
+          </span>
+          Log out
         </button>
       </div>
     </div>
