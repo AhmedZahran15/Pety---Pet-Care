8d61030c0e0b511f645c8636b82f4ab53396726f
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-04 20:31:53 +0200

    Refactor PetWorkerCard component

--- src/Pages/Services/PetWorkerCard.jsx
+++ src/Pages/Services/PetWorkerCard.jsx
@@ -1,81 +1,12 @@
-import Star from "../../components/Star";
 import TimeTable from "../../components/TimeTable";
 import PropTypes from "prop-types";
-function UserInfo({ src, alt, text }) {
-  return (
-    <div className="flex items-center gap-1 text-sm font-normal text-gray-500">
-      <img src={src} alt={alt} className="h-4 w-4" />
-      <span>{text}</span>
-    </div>
-  );
-}
-UserInfo.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-};
+import WorkerInfo from "../../components/WorkerInfo";
+
 function PetWorkerCard({ data }) {
-  const {
-    _id,
-    petyName,
-    averageRate,
-    description,
-    price,
-    availabilityFormatted: availability,
-    animals,
-  } = data;
-  const animalsString = animals
-    .map((item, i) => {
-      if (i === animals.length - 1)
-        return item[0].toUpperCase() + item.substring(1) + "s";
-      return item[0].toUpperCase() + item.substring(1) + "s / ";
-    })
-    .join("");
   return (
-    <div className="flex w-full min-w-[370px] cursor-pointer flex-col items-start justify-center gap-x-8 gap-y-6 rounded-xl border-[0.5px] border-black border-opacity-20 bg-white py-4 shadow-lg shadow-neutral-300 hover:bg-neutral-50 md:flex-row lg:flex-col xl:flex-row">
-      <div className="flex w-full items-center justify-center gap-6 self-center px-6">
-        <img
-          src="images/vetImg.jpg"
-          alt="pet worker"
-          className="h-32 w-32 rounded-full shadow-md shadow-gray-400"
-        />
-        <div className="flex flex-col gap-1">
-          <h2 className=" text-2xl font-bold first-letter:capitalize">
-            {petyName}
-          </h2>
-          <div className="flex">
-            {Array.from({ length: 5 }, (_, i) => (
-              <Star key={i} full={i + 1 <= averageRate} />
-            ))}
-          </div>
-          <UserInfo
-            src="images/filters/filterArrow.png"
-            alt="Description Icon"
-            text={description}
-          />
-          <UserInfo
-            src="images/filters/markerIcon.png"
-            alt="Marker Icon"
-            text="Location PlaceHolder"
-          />
-          <UserInfo
-            src="images/filters/dollarIcon.png"
-            alt="Dollar Icon"
-            text={`Fees: ${price} LE`}
-          />
-          <UserInfo
-            src="images/filters/Clock.png"
-            alt="Clock Icon"
-            text="Waiting Time placeHolder"
-          />
-          <UserInfo
-            src="images/filters/animalsIcon.png"
-            alt="Animals Icon"
-            text={animalsString}
-          />
-        </div>
-      </div>
-      <TimeTable availability={availability} id={_id} />
+    <div className="flex w-full cursor-pointer flex-col items-start justify-center gap-x-8 gap-y-6 rounded-xl border-[0.5px] border-black border-opacity-20 bg-white py-4 shadow-lg shadow-neutral-300 hover:bg-neutral-50 md:flex-row lg:flex-col xl:flex-row">
+      <WorkerInfo data={data} />
+      <TimeTable data={data} />
     </div>
   );
 }
