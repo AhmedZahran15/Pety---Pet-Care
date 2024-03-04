35338cc602143fbe25a7b6951a25f154f6bae838
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-04 20:33:50 +0200

    Add animalsToString function to helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,13 @@
 export function scrollToTopOfElement(elementRef) {
   elementRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
 }
+
+export function animalsToString(animals) {
+  return animals
+    .map((item, i) => {
+      if (i === animals.length - 1)
+        return item[0].toUpperCase() + item.substring(1) + "s";
+      return item[0].toUpperCase() + item.substring(1) + "s / ";
+    })
+    .join("");
+}
