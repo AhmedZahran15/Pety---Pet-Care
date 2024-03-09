647cb65feea858a51f39be29fe0c2d7253b1ed65
Ahmed Zhran <ahmedzhran876@gmail.com>
2024-03-09 20:02:23 +0200

    Remove unused imports and useEffect in BecomeAPety component

--- src/Pages/BecomeAPety/index.jsx
+++ src/Pages/BecomeAPety/index.jsx
@@ -1,26 +1,15 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import BecomeAPetyContext from "../../contexts/BecomeAPetyContext";
 import StepOne from "./StepOne";
 import StepTwo from "./StepTwo";
 import StepThree from "./StepThree";
 import ProgressBar from "./ProgressBar";
-import AuthContext from "../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
+
 function BecomeAPety() {
-  const navigate = useNavigate();
   const {
     state: { step },
   } = useContext(BecomeAPetyContext);
-  const { user } = useContext(AuthContext);
-  useEffect(() => {
-    if (!user) {
-      toast.error("You need to be logged in to access this page", {
-        id: "login-error-toast",
-      });
-      navigate("/auth/login");
-    }
-  }, [user, navigate]);
+
   return (
     <div className="flex h-full w-full flex-col  items-center bg-neutral-100 px-6 py-24">
       <ProgressBar step={step} />
