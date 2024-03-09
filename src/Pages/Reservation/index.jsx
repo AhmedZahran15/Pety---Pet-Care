d67f2228b113855f1c5f2da02ee2f9bb7d5faaa3
Ahmed Zhran <ahmedzhran876@gmail.com>
2024-03-09 20:02:47 +0200

    Remove unused code and update UI layout in Reservation page

--- src/Pages/Reservation/index.jsx
+++ src/Pages/Reservation/index.jsx
@@ -1,24 +1,26 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { ReservationContext } from "../../contexts/ReservationContext";
-import { useNavigate } from "react-router-dom";
 import WorkerInfo from "../../components/WorkerInfo";
 import ReservationTime from "./ReservationTime";
+import { Navigate } from "react-router-dom";
 
 function Reservation() {
   const { appointment } = useContext(ReservationContext);
-  const navigate = useNavigate();
-  useEffect(() => {
-    console.log(appointment.data);
-    if (!appointment) {
-      navigate("/");
-    }
-  }, [appointment, navigate]);
+
+  if (appointment === null) {
+    return <Navigate to="/" replace />;
+  }
 
   return (
-    <div className="bg-neutral-100 px-8 py-10 lg:px-32">
+    <div className="flex flex-col gap-8 bg-neutral-100 px-8 py-10 lg:flex-row lg:px-32">
       <div className="w-full rounded-xl bg-white py-2 md:w-7/12 lg:w-6/12">
         <WorkerInfo data={appointment.data} />
         <ReservationTime appointment={appointment} />
+      </div>
+      <div className="w-full overflow-hidden rounded-xl bg-white md:w-7/12 lg:w-6/12">
+        <div className="bg-primary py-1 text-center font-sans text-lg font-semibold text-white ">
+          Appointment details
+        </div>
       </div>
     </div>
   );
