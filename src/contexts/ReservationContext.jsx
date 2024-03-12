13187314831c0cc433add8500230680e26556f86
Ahmed Zhran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-12 03:06:11 +0200

    Add handleReservation function to ReservationProvider

--- src/contexts/ReservationContext.jsx
+++ src/contexts/ReservationContext.jsx
@@ -1,14 +1,39 @@
+const API_URL = "https://petcare-znql.onrender.com";
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
 export const ReservationContext = createContext();
 
 export const ReservationProvider = ({ children }) => {
   const [appointment, setAppointment] = useState(null);
-  function handleReservation() {
-    console.log("Reservation");
-  }
+  const [isLoading, setIsLoading] = useState(false);
+  const handleReservation = async (petyID, date, time, animals) => {
+    const token = localStorage.getItem("token");
+    const response = await fetch(`${API_URL}/api/pety/appointment`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        petyID,
+        date,
+        time,
+        animals,
+      }),
+    });
+    const data = await response.json();
+    return data;
+  };
   return (
-    <ReservationContext.Provider value={{ appointment, setAppointment }}>
+    <ReservationContext.Provider
+      value={{
+        appointment,
+        setAppointment,
+        handleReservation,
+        isLoading,
+        setIsLoading,
+      }}
+    >
       {children}
     </ReservationContext.Provider>
   );
