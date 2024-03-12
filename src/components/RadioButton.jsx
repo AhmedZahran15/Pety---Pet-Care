57da6b86b42e3a0d3c7bb4f2f6b92b0c97fb180c
Ahmed Zhran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-12 03:05:25 +0200

    Refactor RadioButton component to use flexbox layout

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 function RadioButton({ value, checked, name, id, label, onChange }) {
   return (
-    <div>
+    <div className="flex flex-row items-center gap-2">
       <input
+        className="h-4 w-4"
         type="radio"
         id={id}
         checked={checked}
@@ -10,7 +11,7 @@ function RadioButton({ value, checked, name, id, label, onChange }) {
         name={name}
         onChange={onChange}
       />
-      <label className="ml-1 text-gray-600" htmlFor={id}>
+      <label className="text-gray-600" htmlFor={id}>
         {label}
       </label>
     </div>
