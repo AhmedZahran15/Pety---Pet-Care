502dc55f24f816dcaff8361701da7ac96a525404
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-15 01:14:34 +0200

    Add target and rel attributes to SocialLink component

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -3,6 +3,8 @@ export const SocialLink = ({ href, src, alt }) => {
   return (
     <a
       className="w-[28px] transition-all duration-200 hover:scale-125"
+      target="_blank"
+      rel="noreferrer"
       href={href}
     >
       <img src={src} alt={alt} />
