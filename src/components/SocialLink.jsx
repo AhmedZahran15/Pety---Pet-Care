3e84a9c226dbbd8409fd378b7424ebfa844c0006
Ahmed Zhran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-14 05:37:22 +0200

    Add SocialLink component

---
+++ src/components/SocialLink.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+export const SocialLink = ({ href, src, alt }) => {
+  return (
+    <a
+      className="w-[28px] transition-all duration-200 hover:scale-125"
+      href={href}
+    >
+      <img src={src} alt={alt} />
+    </a>
+  );
+};
+SocialLink.propTypes = {
+  href: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
