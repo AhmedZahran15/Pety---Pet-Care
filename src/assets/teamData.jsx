afe1542273b2a8151ad343365388b4385b076c7b
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-15 01:14:26 +0200

    Update team members data

--- src/assets/teamData.jsx
+++ src/assets/teamData.jsx
@@ -2,37 +2,61 @@ export const teamData = [
   {
     name: "Omar Ghazy",
     role: "Leader/ Data scientist",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/OmarGhazy.jpeg",
     links: [
-      ["#", "images/Github.svg", "Github"],
-      ["#", "images/linkedIn.svg", "linkedIn"],
-      ["#", "images/Facebook.svg", "Facebook"],
+      ["https://github.com/omarghazy56", "images/Github.svg", "Github"],
+      [
+        "https://www.linkedin.com/in/omar-ghazy-6ab478216/",
+        "images/linkedIn.svg",
+        "linkedIn",
+      ],
+      [
+        "https://www.facebook.com/omar.ibrahim.397/",
+        "images/Facebook.svg",
+        "Facebook",
+      ],
     ],
   },
   {
     name: "Ahmed Metwally",
     role: "Data scientist",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/malePlaceHolder.jpg",
     links: [
-      ["#", "images/Github.svg", "Github"],
-      ["#", "images/linkedIn.svg", "linkedIn"],
-      ["#", "images/Facebook.svg", "Facebook"],
+      ["https://github.com/meto2001", "images/Github.svg", "Github"],
+      [
+        "https://www.linkedin.com/in/ahmed-mettwally-80800b203/",
+        "images/linkedIn.svg",
+        "linkedIn",
+      ],
+      [
+        "https://www.facebook.com/ahmed.mohamedmettwally/",
+        "images/Facebook.svg",
+        "Facebook",
+      ],
     ],
   },
   {
     name: "Abdalrahman Aboalnaga",
     role: "Security Engineer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/AbdElrahmanAboAlnaga.png",
     links: [
       ["#", "images/Github.svg", "Github"],
-      ["#", "images/linkedIn.svg", "linkedIn"],
-      ["#", "images/Facebook.svg", "Facebook"],
+      [
+        "https://www.linkedin.com/in/abdalrhman-aboalnaga-73a381260/",
+        "images/linkedIn.svg",
+        "linkedIn",
+      ],
+      [
+        "https://www.facebook.com/abdalrhman.aboalnaga.1/",
+        "images/Facebook.svg",
+        "Facebook",
+      ],
     ],
   },
   {
     name: "Ahmed Zahran",
     role: "FrontEnd Developer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/AhmedZahran.jpg",
     links: [
       ["https://github.com/AhmedZahran15", "images/Github.svg", "Github"],
       [
@@ -50,9 +74,9 @@ export const teamData = [
   {
     name: "Kareem Azzam",
     role: "FrontEnd Developer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/malePlaceHolder.jpg",
     links: [
-      ["#", "images/Github.svg", "Github"],
+      ["https://github.com/unlckyzeta", "images/Github.svg", "Github"],
       ["#", "images/linkedIn.svg", "linkedIn"],
       ["#", "images/Facebook.svg", "Facebook"],
     ],
@@ -60,48 +84,80 @@ export const teamData = [
   {
     name: "Amr Sherif",
     role: "UI/UX Designer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/malePlaceHolder.jpg",
     links: [
       ["#", "images/Github.svg", "Github"],
-      ["#", "images/linkedIn.svg", "linkedIn"],
-      ["#", "images/Facebook.svg", "Facebook"],
+      [
+        "https://www.linkedin.com/in/amr-sherif-a90509247/",
+        "images/linkedIn.svg",
+        "linkedIn",
+      ],
+      [
+        "https://www.facebook.com/amr.sherif.5/",
+        "images/Facebook.svg",
+        "Facebook",
+      ],
     ],
   },
 
   {
     name: "Emad Samy",
     role: "BackEnd Developer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/EmadSamy.jpeg",
     links: [
-      ["#", "images/Github.svg", "Github"],
-      ["#", "images/linkedIn.svg", "linkedIn"],
-      ["#", "images/Facebook.svg", "Facebook"],
+      ["https://github.com/emadsamy-cell", "images/Github.svg", "Github"],
+      [
+        "https://www.linkedin.com/in/emad-samy-21837a215/",
+        "images/linkedIn.svg",
+        "linkedIn",
+      ],
+      [
+        "https://www.facebook.com/emad.samy.7503/",
+        "images/Facebook.svg",
+        "Facebook",
+      ],
     ],
   },
   {
     name: "Farah Hezma",
     role: "BackEnd Developer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/femalePlaceHolder.jpg",
     links: [
-      ["#", "images/Github.svg", "Github"],
-      ["#", "images/linkedIn.svg", "linkedIn"],
-      ["#", "images/Facebook.svg", "Facebook"],
+      ["https://github.com/fara77ezma", "images/Github.svg", "Github"],
+      [
+        "https://www.linkedin.com/in/farah-hezma-b45363207",
+        "images/linkedIn.svg",
+        "linkedIn",
+      ],
+      [
+        "https://www.facebook.com/fo.fa.7528?mibextid=rS40aB7S9Ucbxw6v",
+        "images/Facebook.svg",
+        "Facebook",
+      ],
     ],
   },
   {
     name: "Abdelmohymn ElBashier",
     role: "Flutter Developer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/Abdelmohymn.jpeg",
     links: [
-      ["#", "images/Github.svg", "Github"],
-      ["#", "images/linkedIn.svg", "linkedIn"],
-      ["#", "images/Facebook.svg", "Facebook"],
+      ["https://github.com/Abdelmohaymn", "images/Github.svg", "Github"],
+      [
+        "https://www.linkedin.com/in/abdelmohaymn-elbashier-4146b0235/",
+        "images/linkedIn.svg",
+        "linkedIn",
+      ],
+      [
+        "https://www.facebook.com/abd.elmohaymn/",
+        "images/Facebook.svg",
+        "Facebook",
+      ],
     ],
   },
   {
     name: "Mady",
     role: "Flutter Developer",
-    photo: "images/AboutUs/king.jpg",
+    photo: "images/AboutUs/teamPictures/femalePlaceHolder.jpg",
     links: [
       ["#", "images/Github.svg", "Github"],
       ["#", "images/linkedIn.svg", "linkedIn"],
