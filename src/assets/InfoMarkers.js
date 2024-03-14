237cd6f13e319bab9721a93efa6649b2847b98d0
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-15 00:12:21 +0200

    Add infoMarkers data

---
+++ src/assets/InfoMarkers.js
@@ -0,0 +1,23 @@
+const infoMarkers = [
+  {
+    src: "images/filters/filterArrow.png",
+    alt: "Description Icon",
+  },
+  {
+    src: "images/filters/markerIcon.png",
+    alt: "Marker Icon",
+  },
+  {
+    src: "images/filters/dollarIcon.png",
+    alt: "Dollar Icon",
+  },
+  {
+    src: "images/filters/Clock.png",
+    alt: "Clock Icon",
+  },
+  {
+    src: "images/filters/animalsIcon.png",
+    alt: "Animals Icon",
+  },
+];
+export default infoMarkers;
