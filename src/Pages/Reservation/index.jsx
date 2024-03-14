2401fd8e5d7ffd3a18dd9cbd9e4928b592b9cc06
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-15 00:14:05 +0200

    fix responsive design

--- src/Pages/Reservation/index.jsx
+++ src/Pages/Reservation/index.jsx
@@ -14,12 +14,12 @@ function Reservation() {
   }
 
   return (
-    <div className="flex flex-col gap-8 bg-neutral-100 px-8 py-10 lg:flex-row lg:px-32">
-      <div className="h-fit w-full rounded-xl bg-white py-2 md:w-7/12 lg:w-6/12">
+    <div className="flex flex-col gap-8 bg-neutral-100 px-8 py-10 md:px-32 lg:flex-row lg:px-16 xl:px-32">
+      <div className="h-fit w-full rounded-xl bg-white py-2 lg:w-6/12">
         <WorkerInfo data={appointment.data} />
         <ReservationTime appointment={appointment} />
       </div>
-      <div className="flex w-full flex-col overflow-hidden rounded-xl bg-white md:w-7/12 lg:w-6/12">
+      <div className="flex w-full flex-col overflow-hidden rounded-xl bg-white lg:w-6/12">
         <div className="border-b-[3px] border-neutral-200">
           <div className="bg-primary py-1 text-center font-sans text-lg font-bold text-white ">
             Appointment details
