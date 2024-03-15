412b87c44b4a3687562a470c0181ce4a3807e4c3
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-03-15 05:15:55 +0200

    add shadows to containers

--- src/Pages/Reservation/index.jsx
+++ src/Pages/Reservation/index.jsx
@@ -15,11 +15,11 @@ function Reservation() {
 
   return (
     <div className="flex flex-col gap-8 bg-neutral-100 px-8 py-10 md:px-32 lg:flex-row lg:px-16 xl:px-32">
-      <div className="h-fit w-full rounded-xl bg-white py-2 lg:w-6/12">
+      <div className="h-fit w-full rounded-xl bg-white py-2 shadow-lg shadow-neutral-300 lg:w-6/12">
         <WorkerInfo data={appointment.data} />
         <ReservationTime appointment={appointment} />
       </div>
-      <div className="flex w-full flex-col overflow-hidden rounded-xl bg-white lg:w-6/12">
+      <div className="flex w-full flex-col overflow-hidden rounded-xl bg-white shadow-lg shadow-neutral-300 lg:w-6/12">
         <div className="border-b-[3px] border-neutral-200">
           <div className="bg-primary py-1 text-center font-sans text-lg font-bold text-white ">
             Appointment details
