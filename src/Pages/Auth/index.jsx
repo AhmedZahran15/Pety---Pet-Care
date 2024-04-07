f584f1e5e0ecafeb3b0b2dc374ab163da18d7bf1
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-07 09:02:56 +0200

    Update Auth component styling

--- src/Pages/Auth/index.jsx
+++ src/Pages/Auth/index.jsx
@@ -9,7 +9,7 @@ function Auth() {
     navigate("/");
   }
   return (
-    <div className="relative box-border flex w-screen overflow-hidden bg-gradient-to-t from-[#eaeaea] to-[#f3f3f3] bg-contain bg-right bg-no-repeat p-4 sm:p-10 md:h-screen">
+    <div className="relative box-border flex min-h-screen w-screen overflow-hidden bg-gradient-to-t from-[#eaeaea] to-[#f3f3f3] bg-contain bg-right bg-no-repeat p-4 sm:p-10">
       <Outlet />
       <div className="absolute right-0 z-[0] -mt-10 hidden h-screen md:block">
         <img className="h-screen" src="/bg.png" alt="Auth Image" />
