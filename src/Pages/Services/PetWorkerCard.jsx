b2a1540aa8a9e89d0092522ef1704eb5c8df7806
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-18 02:17:03 +0200

    Update gap between info and timeTable

--- src/Pages/Services/PetWorkerCard.jsx
+++ src/Pages/Services/PetWorkerCard.jsx
@@ -4,7 +4,7 @@ import WorkerInfo from "../../components/WorkerInfo";
 
 function PetWorkerCard({ data }) {
   return (
-    <div className="flex w-full cursor-pointer flex-col items-start justify-center gap-x-8 gap-y-6 rounded-xl border-[0.5px] border-black border-opacity-20 bg-white px-4 py-4 shadow-lg shadow-neutral-300 hover:bg-neutral-50 lg:flex-col xl:flex-row">
+    <div className="flex w-full cursor-pointer flex-col items-start justify-center gap-x-2 gap-y-6 rounded-xl border-[0.5px] border-black border-opacity-20 bg-white px-4 py-4 shadow-lg shadow-neutral-300 hover:bg-neutral-50 lg:flex-col xl:flex-row">
       <WorkerInfo data={data} />
       <TimeTable data={data} />
     </div>
