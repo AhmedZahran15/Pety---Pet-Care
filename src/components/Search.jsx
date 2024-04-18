499fd52ca7c229db4e2b6ef3a4d44212b5a13a97
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-18 03:20:31 +0200

    implement search wih location component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,72 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { cities, governorates } from "../assets/governoratesData";
+import { useNavigate } from "react-router-dom";
+import SearchDropdown from "./SearchDropdown";
+
 const Search = () => {
+  const [role, setRole] = useState("Veterinarian");
+  const [governorate, setGovernorate] = useState({ id: 0, name: "" });
+  const [city, setCity] = useState({ id: 0, name: "" });
+  const areas = cities.filter((city) => city.governorate_id === governorate.id);
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    const roleValue =
+      role === "Veterinarian"
+        ? "vet"
+        : role === "Pet groomer"
+          ? "groomer"
+          : "petSitter";
+    if (!city.id && !governorate.id) {
+      navigate(`services?role=${roleValue}`);
+    }
+    if (city.id && !governorate.id) {
+      navigate(`services?role=${roleValue}&city=${city.name}`);
+    }
+    if (city.id && governorate.id) {
+      navigate(
+        `services?role=${roleValue}&city=${city.name}&governorate=${governorate.name}`,
+      );
+    }
+    if (!city.id && governorate.id) {
+      navigate(`services?role=${roleValue}&governorate=${governorate.name}`);
+    }
+  };
   return (
-    <div className="sm:px-30 mt-6 flex flex-col px-20 lg:px-32">
-      <div className="mb-16 mt-5 flex w-full flex-col items-center gap-8  lg:flex-row">
-        <select className="w-full basis-1/3 cursor-pointer rounded-xl border-2 border-neutral-400  py-3 text-left text-2xl focus:border-secondary ">
-          <option className="hidden text-xl font-normal text-neutral-500">
-            What are you looking for?
-          </option>
-          <option className="text-xl font-normal text-neutral-500">Vet</option>
-          <option className=" text-xl font-normal text-neutral-500">
-            Pet Setting
-          </option>
-          <option className=" text-xl font-normal text-neutral-500">
-            Pet Grooming
-          </option>
-        </select>
-        <select className="w-full  basis-1/3 cursor-pointer rounded-xl border-2 border-neutral-400 py-3 text-left  text-2xl focus:border-secondary ">
-          <option className="text-xl font-normal text-neutral-500">
-            Select your location
-          </option>
-        </select>
-        <button className="w-full basis-1/3  rounded-xl bg-secondary py-3 text-center text-3xl font-bold text-white hover:bg-amber-500 ">
+    <div className="mx-8 mt-6 rounded-xl  border-[1px] border-neutral-300 shadow-xl shadow-neutral-300  sm:container sm:mx-auto">
+      <div className=" flex flex-col items-center rounded-xl  md:flex-row">
+        <div className="flex w-full basis-3/4 flex-col divide-y-2 divide-neutral-200 rounded-l-xl md:flex-row md:divide-x-2 md:divide-y-0">
+          <SearchDropdown
+            title="Select a specialty"
+            placeholder="Veterinarian"
+            iconSrc="stethoscope.png"
+            state={role}
+            setState={setRole}
+            data={["Veterinarian", "Pet groomer", "Pet Sitter"]}
+          />
+          <SearchDropdown
+            title="In this city"
+            placeholder="All cities"
+            iconSrc="images/filters/markerIcon.png"
+            state={governorate}
+            setState={setGovernorate}
+            data={governorates}
+          />
+          <SearchDropdown
+            title="In this area"
+            placeholder="All areas"
+            iconSrc="images/filters/markerIcon.png"
+            state={city}
+            setState={setCity}
+            data={areas}
+          />
+        </div>
+        <button
+          className="flex h-20 min-h-[60px] w-full  basis-1/4 items-center justify-center gap-x-4 rounded-b-xl bg-secondary text-3xl font-normal text-white hover:bg-amber-500 md:rounded-r-xl md:rounded-bl-none"
+          onClick={handleSearch}
+        >
+          <img className="mt-1 h-8 w-8" src="searchIcon.png" alt="" />
           Search
         </button>
       </div>
