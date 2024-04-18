f1859c966c5be767269fade05072956b79cf76b4
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-18 22:16:39 +0200

    Refactor useUpdateEffect hook to handle 3 renders

--- src/Hooks/useUpdateEffect.jsx
+++ src/Hooks/useUpdateEffect.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 export default function useUpdateEffect(callback, dependencies) {
   const count = useRef(0);
   useEffect(() => {
-    if (count.current < 2) {
+    if (count.current <= 2) {
       count.current++;
       return;
     }
