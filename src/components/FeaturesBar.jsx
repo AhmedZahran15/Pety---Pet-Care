8fae1df736b3d9e59eb2454e707e3f005502bd88
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-19 23:59:16 +0200

    use group styling and transition animation

--- src/components/FeaturesBar.jsx
+++ src/components/FeaturesBar.jsx
@@ -1,35 +1,53 @@
 import { Link, useSearchParams } from "react-router-dom";
-import PetSittingSvg from "../assets/PetSittingSvg.jsx";
 import VetSvg from "../assets/VetSvg.jsx";
 import PetGroomingSvg from "../assets/PetGroomingSvg.jsx";
 import PropTypes from "prop-types";
+import PetSitter from "../assets/PetSitterSVG.jsx";
 const FeaturesBar = ({ imgSrc }) => {
   const [filterParams] = useSearchParams();
   const role = filterParams ? filterParams.get("role") : "";
   return (
     <>
       <img src={imgSrc} alt="Header image" className="w-full  bg-contain" />
-      <div className="sm:container mx-8 sm:mx-auto -mt-16 grid grid-cols-2 items-center rounded-3xl bg-white px-5 py-4 shadow-xl md:grid-cols-4">
+      <div className="mx-8 -mt-16 grid grid-cols-2 items-center rounded-3xl bg-white px-5 py-4 shadow-xl sm:container sm:mx-auto md:grid-cols-4">
         <Link
           to="/services?role=vet"
-          className={`mx-auto flex h-24 w-36 flex-col items-center justify-center rounded-lg px-12 hover:bg-neutral-100 ${role === "vet" ? "bg-neutral-100" : ""}`}
+          className={`group mx-auto flex h-24 w-36 flex-col items-center justify-center rounded-lg px-12 hover:bg-neutral-100 ${role === "vet" ? "bg-neutral-100" : ""}`}
         >
-          <VetSvg />
-          <p className={`text-center text-xl text-primary`}>Vet</p>
+          <VetSvg
+            className={`transition-all duration-200 group-hover:fill-secondary ${role === "vet" ? "fill-secondary" : ""}`}
+          />
+          <p
+            className={`text-center text-xl transition-all duration-200 group-hover:text-secondary ${role === "vet" ? "text-secondary" : ""}`}
+          >
+            Vet
+          </p>
         </Link>
         <Link
           to="/services?role=petSitter"
-          className={`mx-auto flex h-24 w-36 flex-col items-center justify-center rounded-lg hover:bg-neutral-100 ${role === "petSitter" ? "bg-neutral-100" : ""}`}
+          className={`group mx-auto flex h-24 w-36 flex-col items-center justify-center rounded-lg hover:bg-neutral-100 ${role === "petSitter" ? "bg-neutral-100" : ""}`}
         >
-          <PetSittingSvg />
-          <p className={`text-center text-xl text-black`}>Pet Sitting</p>
+          <PetSitter
+            className={`${role === "petSitter" ? "stroke-secondary" : "stroke-black"} h-16 w-16 transition-all duration-200 group-hover:stroke-secondary`}
+          />
+          <p
+            className={`text-center text-xl transition-all duration-200 group-hover:text-secondary ${role === "petSitter" ? "text-secondary" : ""}`}
+          >
+            Pet Sitting
+          </p>
         </Link>
         <Link
           to="/services?role=groomer"
-          className={`mx-auto flex h-24 w-36 flex-col items-center justify-center rounded-lg hover:bg-neutral-100 ${role === "groomer" ? "bg-neutral-100" : ""}`}
+          className={`group mx-auto flex h-24 w-36 flex-col items-center justify-center rounded-lg hover:bg-neutral-100 ${role === "groomer" ? "bg-neutral-100" : ""}`}
         >
-          <PetGroomingSvg />
-          <p className={`text-center text-xl text-secondary`}>Pet Grooming</p>
+          <PetGroomingSvg
+            className={`${role === "groomer" ? "fill-secondary" : "fill-black"} h-16 w-16 transition-all duration-200 group-hover:fill-secondary`}
+          />
+          <p
+            className={`text-center text-xl transition-all duration-200 group-hover:text-secondary ${role === "groomer" ? "text-secondary" : ""}`}
+          >
+            Pet Grooming
+          </p>
         </Link>
         <div className="mx-auto flex h-24 w-36 cursor-pointer flex-col items-center justify-between rounded-lg  hover:bg-neutral-100">
           <svg
