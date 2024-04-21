033755a598573975dd3491ad216876611b47cdc9
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-21 05:54:11 +0200

    Refactor UserDropDown component and update styles

--- src/components/UserDropDown.jsx
+++ src/components/UserDropDown.jsx
@@ -21,38 +21,42 @@ function UserDropDown() {
   if (!userData) return null;
   return (
     <div className="userDropDown relative inline-block text-left">
-      <div>
-        <button
-          onClick={() => setIsOpen((prev) => !prev)}
-          type="button"
-          className="inline-flex items-center justify-center gap-x-4 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          <div className="flex items-center gap-x-1">
-            <img
-              src="images/defaultWorkerImage.png"
-              alt="avatar"
-              className="h-8 w-8 rounded-full"
-            />
-            <p>{userData?.firstName}</p>
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        type="button"
+        className="flex items-center justify-center gap-x-4 border-l-2 border-neutral-300 bg-white px-3 text-neutral-900"
+      >
+        <div className="flex items-center gap-x-4">
+          <div className="flex flex-col">
+            <span className="text-sm font-medium">
+              {userData?.firstName} {userData.lastName}
+            </span>
+            <span className="text-xs font-normal text-neutral-400">
+              Service Provider
+            </span>
           </div>
-          <svg
-            className="-mr-1 h-5 w-5 text-gray-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-      </div>
+          <span className="max-h-[48px] max-w-[48px] overflow-clip rounded-full">
+            <img src="userImage.png" alt="avatar" />
+          </span>
+        </div>
+        <svg
+          className="-mr-1 h-5 w-5 text-neutral-800"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
       <div
         className={`${
           isOpen ? "block" : "hidden"
-        } absolute right-[-1] z-10 mt-2 w-36 origin-top-right overflow-clip rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+        } absolute right-[-1] z-10 mt-2 w-full origin-top-right overflow-clip bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
       >
         <Link
           to="/dashboard"
