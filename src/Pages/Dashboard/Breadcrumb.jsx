d0d1c570105a4e5031f0e35110fbce3d0cb8edeb
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-22 06:38:30 +0200

    Add Breadcrumb component to Dashboard page

---
+++ src/Pages/Dashboard/Breadcrumb.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+const Breadcrumb = ({ pageName }) => {
+  return (
+    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+      <h2 className="text-[26px] font-semibold text-black ">{pageName}</h2>
+
+      <nav>
+        <ol className="flex items-center gap-2 text-[16px]">
+          <li>
+            <Link className="font-medium" to="/dashboard">
+              Dashboard /
+            </Link>
+          </li>
+          <li className="font-medium text-primary">{pageName}</li>
+        </ol>
+      </nav>
+    </div>
+  );
+};
+Breadcrumb.propTypes = {
+  pageName: PropTypes.string,
+};
+export default Breadcrumb;
