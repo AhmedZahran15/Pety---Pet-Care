16ee1ccf9cc9d43824964b7841dd7a4e3949610b
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-22 21:20:42 +0200

    Update Reservations.jsx to fetch and display appointments based on user role

--- src/Pages/Dashboard/Reservations.jsx
+++ src/Pages/Dashboard/Reservations.jsx
@@ -1,9 +1,47 @@
-import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { BlockLoader } from "../../components/Loader";
 function Reservations() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [reservations, setReservations] = useState([]);
   const { pathname } = useLocation();
   const role = pathname.split("/")[2];
-  return <div></div>;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `${import.meta.env.VITE_API_LINK}/api/dashboard/allAppointments/`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+            body: JSON.stringify({ role }),
+          },
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setReservations(data.data);
+          console.log(data.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, [role]);
+  return (
+    <>
+      {isLoading ? (
+        <BlockLoader />
+      ) : (
+        <div className="relative mx-auto w-full"></div>
+      )}
+    </>
+  );
 }
 
 export default Reservations;
