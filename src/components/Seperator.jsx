160f697366882d56000b8c2ac9d253ac1a5175ce
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-22 20:38:18 +0200

    Remove Separator component

--- src/components/Seperator.jsx
+++
@@ -1,9 +0,0 @@
-export default function Separator() {
-  return (
-    <div className="flex flex-row items-center">
-      <hr className="h-[2px] w-1/2 bg-gray-300" />
-      <span className=" mx-1 text-lg font-semibold">OR</span>
-      <hr className="h-[2px] w-1/2 bg-gray-300" />
-    </div>
-  );
-}
