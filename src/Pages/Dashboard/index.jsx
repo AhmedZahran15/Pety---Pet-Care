7ac5d52be36194163bd7069b18146665c948a8a0
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-22 21:18:52 +0200

    Refactor Dashboard component to improve responsiveness

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,28 +1,23 @@
 import { useState } from "react";
-
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
-const Dashboard = ({ children }) => {
+const Dashboard = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div>
       <div className="flex h-screen overflow-hidden">
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <div className="relative flex min-h-screen max-w-screen-2xl flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <main>
-            <div className="mx-auto h-screen max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              <Outlet />
-            </div>
+          <main className="relative flex h-full flex-1 p-4 md:p-6 2xl:p-10">
+            <Outlet />
           </main>
         </div>
       </div>
     </div>
   );
 };
-
 export default Dashboard;
