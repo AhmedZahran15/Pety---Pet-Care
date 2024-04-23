8cd171ba10b930ebe5f502b554aa6ae4148c2629
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-23 04:54:14 +0200

    change text color

--- src/Pages/Dashboard/Breadcrumb.jsx
+++ src/Pages/Dashboard/Breadcrumb.jsx
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 const Breadcrumb = ({ pageName }) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      <h2 className="text-[26px] font-semibold text-black ">{pageName}</h2>
+      <h2 className="text-[26px] font-semibold text-neutral-900 ">
+        {pageName}
+      </h2>
 
       <nav>
         <ol className="flex items-center gap-2 text-[16px]">
