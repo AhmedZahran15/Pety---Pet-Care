78f6c1e899de433c6b200389fa344f64a04f3f67
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-23 04:54:02 +0200

    add reservations table

--- src/Pages/Dashboard/Reservations.jsx
+++ src/Pages/Dashboard/Reservations.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { BlockLoader } from "../../components/Loader";
+import Breadcrumb from "./Breadcrumb";
+import TableRow from "./TableRow";
+
+const Table_Head = ["ID", "Name", "Phone", "Date", "Time", "Status", "Actions"];
 function Reservations() {
   const [isLoading, setIsLoading] = useState(true);
   const [reservations, setReservations] = useState([]);
@@ -24,7 +28,6 @@ function Reservations() {
         if (response.ok) {
           const data = await response.json();
           setReservations(data.data);
-          console.log(data.data);
         }
       } catch (error) {
         console.error(error);
@@ -33,12 +36,67 @@ function Reservations() {
     }
     fetchData();
   }, [role]);
+
+  const handleStatusChange = async (id, status) => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_LINK}/api/dashboard/changeAppointment`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({ id, role, status }),
+        },
+      );
+      if (response.ok) {
+        const data = await response.json();
+        if (data.status === "success") {
+          const updatedReservations = reservations.map((reservation) => {
+            if (reservation._id === id) {
+              reservation.status = status;
+            }
+            return reservation;
+          });
+          setReservations(updatedReservations);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       {isLoading ? (
         <BlockLoader />
       ) : (
-        <div className="relative mx-auto w-full"></div>
+        <div className="relative mx-auto w-full">
+          <Breadcrumb pageName={"Reservations"} />
+          <div className="overflow-x-auto rounded-lg border border-neutral-200">
+            <table className=" font- w-full min-w-max table-auto  rounded-lg text-center text-base font-medium text-neutral-700">
+              <thead className="bg-neutral-100 text-primary">
+                <tr>
+                  {Table_Head.map((head, index) => (
+                    <th key={index} className="px-2 py-3">
+                      {head}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className="divide-y-2 divide-neutral-100">
+                {reservations.map((reservation, index) => (
+                  <TableRow
+                    key={reservation._id}
+                    reservation={reservation}
+                    index={index}
+                    handleStatusChange={handleStatusChange}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       )}
     </>
   );
