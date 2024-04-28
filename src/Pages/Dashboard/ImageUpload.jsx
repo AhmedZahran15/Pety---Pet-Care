bb741fd1da6f49778a779d25b5d777edca650078
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-29 01:15:29 +0300

    Refactor ImageUpload component in Dashboard

--- src/Pages/Dashboard/ImageUpload.jsx
+++ src/Pages/Dashboard/ImageUpload.jsx
@@ -1,5 +1,8 @@
 import PropTypes from "prop-types";
+import { useRef, useState } from "react";
 function ImageUpload({ imageAsPreview, handleImageChange, photo }) {
+  const ref = useRef(null);
+  const [error, setError] = useState("");
   return (
     <div className="flex items-center gap-x-4 md:-mt-3">
       <img
@@ -7,14 +10,30 @@ function ImageUpload({ imageAsPreview, handleImageChange, photo }) {
         alt="profile"
         className="h-[70px] w-[70px] rounded-full"
       />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          ref.current.click();
+        }}
+        className="cursor-pointer rounded-md bg-emerald-200 px-4 py-2 text-sm font-semibold text-primary shadow-md shadow-neutral-300 hover:bg-emerald-300"
+      >
+        Choose Image
+      </button>
+      <span className={`text-neutral-500  ${error ? "hidden" : ""}`}>
+        {photo?.name ? photo.name : "PNG,JPG,JPEG up to 2MB"}
+      </span>
+      <span>{error && <span className="text-red-500">{error}</span>}</span>
       <input
+        ref={ref}
         type="file"
-        onChange={handleImageChange}
-        className="block w-full text-sm file:mr-4
-      file:rounded-md file:border-0 file:bg-emerald-100 file:px-4 file:py-2
-      file:text-sm  file:font-semibold file:text-primary file:shadow-md file:shadow-neutral-300
-       file:hover:bg-emerald-200
-      hover:file:bg-pink-100"
+        accept=".png, .jpg, .jpeg"
+        onChange={(e) => {
+          if (e.target.files && e.target.files[0].size < 2097152) {
+            setError("");
+            handleImageChange(e);
+          } else setError("Image size should be less than 2MB");
+        }}
+        className="hidden"
       />
     </div>
   );
@@ -22,6 +41,6 @@ function ImageUpload({ imageAsPreview, handleImageChange, photo }) {
 ImageUpload.propTypes = {
   imageAsPreview: PropTypes.string,
   handleImageChange: PropTypes.func,
-  photo: PropTypes.string,
+  photo: PropTypes.object,
 };
 export default ImageUpload;
