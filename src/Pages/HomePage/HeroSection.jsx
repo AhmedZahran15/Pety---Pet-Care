132cb05f734af5372be0ca431a6e6f1b1ee5744c
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-29 23:18:11 +0300

    Update styling and layout

--- src/Pages/HomePage/HeroSection.jsx
+++ src/Pages/HomePage/HeroSection.jsx
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom";
 const HeroSection = () => {
   return (
-    <div className="flex h-[105dvh] items-center justify-center overflow-hidden duration-300 md:justify-between  md:bg-bottom  ">
-      <img
-        className="ml-20 hidden w-[37dvw] transition-all duration-300 md:mt-[-35px]  md:block "
-        src="images/homepage/eric-ward-ISg37AI2A-s-unsplash 1.png"
-        alt="cat"
-      />
+    <div className="mx-8 mt-20 flex items-center justify-center gap-x-16 overflow-hidden duration-300 sm:container sm:mx-auto md:justify-between">
+      <div className="hidden transition-all duration-300 md:block md:basis-1/2">
+        <img
+          src="images/homepage/eric-ward-ISg37AI2A-s-unsplash 1.png"
+          alt="Man with cat"
+        />
+      </div>
 
-      <div className="flex h-full flex-col gap-9 px-12 py-28  transition-all duration-300 md:gap-5 lg:px-28 xl:pt-40">
-        <div className="font-[Fredoka]  text-6xl font-bold text-neutral-800 ">
+      <div className="flex h-full flex-col gap-y-6 transition-all duration-300 md:basis-1/2 md:gap-4">
+        <div className="font-fredoka text-3xl font-bold text-neutral-900 sm:text-4xl md:text-5xl ">
           HEALTH <span className="text-secondary">CARE</span> FOR YOUR PET
         </div>
-        <div className=" font-['Montserrat'] text-2xl font-normal text-gray-600">
+        <div className=" font-['Montserrat'] text-xl font-normal text-neutral-600 md:text-2xl">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit porro
           nostrum commodi quidem esse et facere doloribus corrupti veritatis
           repellat, labore consequuntur sapiente, sunt perspiciatis iure
           excepturi ea reprehenderit delectus.
         </div>
-
         <Link
-          className="font-['Roboto Flex'] w-[13rem] rounded-3xl bg-secondary  px-4 py-1.5 text-center font-['Montserrat'] text-2xl font-bold text-white transition-all duration-300  hover:bg-amber-400 "
+          className="w-fit rounded-3xl bg-secondary px-4 py-2 text-center font-['Montserrat'] text-2xl font-bold text-white transition-all duration-300 hover:bg-amber-400 sm:px-12  sm:py-2.5 "
           to="/about"
         >
           Learn More
