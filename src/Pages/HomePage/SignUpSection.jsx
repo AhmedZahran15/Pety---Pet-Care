9b78392995f74263a55ff55b8f7985071ad38942
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-29 23:18:19 +0300

    Update styling and layout

--- src/Pages/HomePage/SignUpSection.jsx
+++ src/Pages/HomePage/SignUpSection.jsx
@@ -2,30 +2,24 @@ import { Link } from "react-router-dom";
 
 const SignUpSection = () => {
   return (
-    <div className="mx-12 mb-10 flex items-center justify-around gap-10 px-8 lg:gap-0">
-      <div className="flex w-full flex-col gap-4 md:w-5/12">
-        <span
-          className="font-['Fredoka'] text-4xl
- font-bold md:text-6xl"
-        >
+    <div className="mx-8 mt-20 flex items-center justify-center gap-x-16 overflow-hidden duration-300 sm:container sm:mx-auto md:justify-between">
+      <div className="flex h-full flex-col gap-y-6 transition-all duration-300 md:basis-1/2 md:gap-4">
+        <span className="font-fredoka text-3xl font-bold text-neutral-900 sm:text-4xl md:text-5xl">
           WHO SHOULD USE PETY?
         </span>
-        <span
-          className="font-['Montserrat'] text-xl font-normal text-gray-600 md:text-4xl
-"
-        >
+        <span className="font-['Montserrat'] text-xl font-normal text-neutral-600 md:text-2xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget
           placerat dui. Sed condimentum tincidunt diam,
         </span>
         <Link
           to="/auth/register"
-          className="my-6 max-w-fit  rounded-full bg-primary px-10 py-3 text-center  font-['Montserrat'] text-3xl font-bold tracking-wide text-white transition-all duration-300 hover:bg-[#015d61] "
+          className="w-fit rounded-3xl bg-primary px-4 py-2 text-center font-['Montserrat'] text-2xl font-bold text-white transition-all duration-300 hover:bg-[#015d61]  sm:px-12  sm:py-2.5 "
         >
           SIGN UP
         </Link>
       </div>
-      <div className=" hidden md:block">
-        <img src="/images/homepage/Vector 3.png" alt="dog" />
+      <div className="hidden transition-all duration-300 md:block md:basis-1/2">
+        <img src="/images/homepage/Vector 3.png" alt="Dog" />
       </div>
     </div>
   );
