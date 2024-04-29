ab3ca1224e3e09501771ffd797550acde3e78179
DaliaZH <121820334+AhmedZahran15@users.noreply.github.com>
2024-04-29 23:17:54 +0300

    Update styling and layout

--- src/Pages/HomePage/CustomerStories.jsx
+++ src/Pages/HomePage/CustomerStories.jsx
@@ -2,11 +2,11 @@ import CustomerStory from "./CustomerStory";
 
 const CustomerStories = () => {
   return (
-    <div className="flex h-full  flex-col justify-center bg-center pb-32   pt-28 xl:bg-[url('/images/homepage/Vector(6).png')]    ">
-      <h1 className="text-center font-['Fredoka'] text-6xl font-bold">
+    <div className="mx-8 mt-20 flex flex-col items-center justify-center gap-y-8 sm:container sm:mx-auto">
+      <h1 className="font-fredoka text-3xl font-bold text-neutral-800 sm:text-4xl md:text-5xl">
         CUSTOMER STORIES
       </h1>
-      <div className="mx-16 my-16 flex flex-col gap-6 md:flex-row lg:mx-40">
+      <div className=" flex flex-col gap-6 lg:flex-row">
         <CustomerStory
           name="Mirage"
           image="https://i.pravatar.cc/50"
@@ -24,7 +24,7 @@ const CustomerStories = () => {
         />
       </div>
 
-      <button className="self-center rounded-full  bg-primary px-20 py-3 text-center text-3xl font-bold text-white transition-all hover:bg-[#015d61]">
+      <button className="w-fit rounded-3xl bg-primary px-4 py-2 text-center font-['Montserrat'] text-2xl font-bold text-white transition-all duration-300 hover:bg-[#015d61]  sm:px-12  sm:py-2.5">
         Read More
       </button>
     </div>
