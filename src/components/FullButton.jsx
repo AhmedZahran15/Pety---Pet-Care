51c09cb841b80dcb20b01e6a4f22c100ef9345e1
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-05-01 00:44:38 +0300

    remove onClick prop

--- src/components/FullButton.jsx
+++ src/components/FullButton.jsx
@@ -3,17 +3,17 @@ import { Loader } from "./Loader";
 import PropTypes from "prop-types";
 import AuthContext from "../contexts/AuthContext";
 
-export const FullButton = ({ text, enabled, onClick }) => {
+export const FullButton = ({ text, enabled }) => {
   const { isLoading } = useContext(AuthContext);
   return (
     <button
+      type="submit"
       disabled={!enabled}
       className={
         "mt-6 w-1/2 self-center rounded-xl px-4 py-2 text-lg text-white  hover:bg-[#1a8588] disabled:bg-gray-300 " +
         (enabled ? "cursor-pointer bg-primary" : "bg-gray-400") +
         (isLoading ? " cursor-not-allowed" : "")
       }
-      onClick={onClick}
     >
       {isLoading && <Loader />} <span>{isLoading ? "Loading..." : text}</span>
     </button>
@@ -23,5 +23,4 @@ export const FullButton = ({ text, enabled, onClick }) => {
 FullButton.propTypes = {
   text: PropTypes.string.isRequired,
   enabled: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
