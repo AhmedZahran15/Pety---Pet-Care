ec0c7187db7dd3a3a977f956b4d54935568baa21
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-05-02 05:11:45 +0300

    Update TeamMember component styling and layout

--- src/Pages/AboutUS/TeamMember.jsx
+++ src/Pages/AboutUS/TeamMember.jsx
@@ -2,15 +2,15 @@ import { SocialLink } from "../../components/SocialLink";
 import PropTypes from "prop-types";
 export default function TeamMember({ name, role, photo, links }) {
   return (
-    <div className="flex flex-col items-center divide-y-2 overflow-hidden rounded-md bg-white">
+    <div className="flex h-fit w-fit flex-col items-center divide-y-2 overflow-hidden rounded-md bg-white">
       <div className="flex flex-col text-center text-sm sm:text-base lg:text-lg">
         <img
           alt={name}
-          className="h-72 min-w-[210px] max-w-[210px]"
+          className="aspect-auto h-80 w-60 object-cover object-top"
           src={photo}
         />
-        <h2 className="font-bold text-neutral-800">{name}</h2>
-        <h3 className="text-neutral-500">{role}</h3>
+        <h2 className="px-2 font-bold text-neutral-800">{name}</h2>
+        <h3 className="px-2 text-neutral-500">{role}</h3>
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
         {links.map((link, index) => (
