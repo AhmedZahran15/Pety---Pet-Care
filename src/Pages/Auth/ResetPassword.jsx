db1473637d48baa2d43f07979816341687646978
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-05-03 11:18:23 +0300

    add ResetPassword component

--- src/Pages/Auth/ResetPassword.jsx
+++ src/Pages/Auth/ResetPassword.jsx
@@ -1,13 +1,139 @@
-import { useParams } from "react-router-dom";
-
+import { useRef, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Modal from "../../components/Modal";
+import toast from "react-hot-toast";
+import { Loader } from "../../components/Loader";
+import {
+  validateConfirmPassword,
+  validatePassword,
+} from "../../utils/validationFunctions";
 function ResetPassword() {
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const dialogRef = useRef(null);
   const { token } = useParams();
-  console.log(token);
+  const navigate = useNavigate();
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+    setPasswordError(validatePassword(e.target.value));
+  }
+  function handleConfirmPasswordChange(e) {
+    setConfirmPassword(e.target.value);
+    setConfirmPasswordError(validateConfirmPassword(password, e.target.value));
+  }
+  function toggleDialog() {
+    if (!dialogRef.current) {
+      return;
+    }
+    dialogRef.current.hasAttribute("open")
+      ? dialogRef.current.close()
+      : dialogRef.current.showModal();
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (passwordError || confirmPasswordError) {
+      toast.error("Please fill the form correctly");
+      return;
+    }
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `${import.meta.env.VITE_API_LINK}/api/users/resetPassword/${token}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ password, passwordConfirm: confirmPassword }),
+        },
+      );
+      const data = await response.json();
+      if (data.status === "success") {
+        toggleDialog();
+        setTimeout(() => {
+          navigate("/auth/login");
+        }, 5000);
+      } else toast.error(data.message);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <div>
-      Reset Password
+    <div className="z-10 flex w-full flex-col gap-6 overflow-y-auto rounded-3xl bg-white px-6 py-6 shadow-md shadow-neutral-400 transition-all duration-300 no-scrollbar sm:px-10 md:w-8/12 lg:p-16 lg:pr-36 xl:pr-64">
+      <Link to="/">
+        <img src="/Logo Placeholder.png" alt="Logo" />
+      </Link>
+      <div>
+        <h2 className="font-fredoka text-4xl font-semibold">
+          Forgot your password ?
+        </h2>
+        <p className="text-base font-medium text-neutral-500">
+          No worries! Enter your email address and we will send you a link to
+          reset your password.
+        </p>
+      </div>
+      <Modal ref={dialogRef} toggleDialog={toggleDialog}>
+        <div className="flex flex-col gap-y-4 p-4">
+          <h2 className="text-lg font-semibold text-neutral-900">
+            You have successfully reset your password
+          </h2>
+          <p>
+            You will be redirected to the login page in 5 seconds. If you are
+            not redirected,&nbsp;
+            <Link className="text-lg font-bold text-primary" to="/auth/login">
+              click here
+            </Link>
+          </p>
+        </div>
+      </Modal>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <div>
+          <input
+            type="password"
+            value={password}
+            required
+            className="w-full rounded-lg border-2 border-neutral-300 p-3 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
+            placeholder="Password"
+            onChange={handlePasswordChange}
+          />
+          {passwordError && (
+            <span className="text-sm text-primary">{passwordError}</span>
+          )}
+        </div>
+        <div>
+          <input
+            type="password"
+            value={confirmPassword}
+            required
+            className="w-full rounded-lg border-2 border-neutral-300 p-3 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
+            placeholder="Confirm Password"
+            onChange={handleConfirmPasswordChange}
+          />
+          {confirmPasswordError && (
+            <span className="text-sm text-primary">{confirmPasswordError}</span>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="w-full max-w-sm self-center rounded-lg bg-primary p-3 font-semibold text-white transition-all duration-300 hover:bg-[#015d61] focus:outline-none focus:ring-2 focus:ring-primary"
+        >
+          {isLoading ? (
+            <div className="flex items-center justify-center gap-x-2">
+              <Loader />
+              <span>Loading...</span>
+            </div>
+          ) : (
+            <span>Submit</span>
+          )}
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default ResetPassword
+export default ResetPassword;
