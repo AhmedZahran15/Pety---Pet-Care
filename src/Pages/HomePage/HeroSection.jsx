ff138f52df562fb17e55297cc3435fdede7f0037
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-05-16 20:45:53 +0300

    refactor: Update HeroSection content for improved pet health messaging

--- src/Pages/HomePage/HeroSection.jsx
+++ src/Pages/HomePage/HeroSection.jsx
@@ -14,10 +14,13 @@ const HeroSection = () => {
           HEALTH <span className="text-secondary">CARE</span> FOR YOUR PET
         </div>
         <div className=" font-['Montserrat'] text-xl font-normal text-neutral-600 md:text-2xl">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit porro
-          nostrum commodi quidem esse et facere doloribus corrupti veritatis
-          repellat, labore consequuntur sapiente, sunt perspiciatis iure
-          excepturi ea reprehenderit delectus.
+          Pet Health Made Easy: From routine check-ups to emergency care, PETY
+          connects you with trusted veterinarians and specialized services,
+          ensuring your pet&apos;s well-being. Explore our platform for
+          convenient scheduling, preventive treatments, and expert advice on
+          nutrition and dental care. With PETY, keeping your furry friend
+          healthy has never been simpler. Start prioritizing your pet&apos;s
+          health today!
         </div>
         <Link
           className="w-fit rounded-3xl bg-secondary px-4 py-2 text-center font-['Montserrat'] text-2xl font-bold text-white transition-all duration-300 hover:bg-amber-400 sm:px-12  sm:py-2.5 "
