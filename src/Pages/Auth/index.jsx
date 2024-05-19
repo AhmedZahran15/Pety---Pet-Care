fb5334b206b1707ee58854eb18666175ded72bc8
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-05-19 17:42:06 +0300

    refactor: fix auth bug

--- src/Pages/Auth/index.jsx
+++ src/Pages/Auth/index.jsx
@@ -1,13 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
 
 function Auth() {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
-  if (user) {
-    navigate("/");
-  }
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
   return (
     <div className="relative box-border flex min-h-screen w-screen items-center overflow-hidden bg-gradient-to-t from-[#eaeaea] to-[#f3f3f3] bg-contain bg-right bg-no-repeat p-4 sm:p-10">
       <Outlet />
