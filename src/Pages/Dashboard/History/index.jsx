faef28c6089d1ecbf47beb61995d7e6ceb1b517c
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-14 03:13:39 +0300

    feat: implement history page

--- src/Pages/Dashboard/History/index.jsx
+++ src/Pages/Dashboard/History/index.jsx
@@ -1,7 +1,19 @@
 import { useEffect, useState } from "react";
 import Breadcrumb from "../Breadcrumb";
 import { BlockLoader } from "../../../components/Loader";
+import TableRow from "./TableRow";
+
+const Table_Head = [
+  "ID",
+  "Photo",
+  "Name",
+  "Role",
+  "Date",
+  "Time",
+  "Status",
+];
 function History() {
+  const [history, setHistory] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +31,8 @@ function History() {
         );
         if (response.ok) {
           const data = await response.json();
-          console.log(data);
+          setHistory(data.data);
+          console.log(data.data);
         }
       } catch (error) {
         console.error(error);
@@ -36,7 +49,35 @@ function History() {
       ) : (
         <div className="relative mx-auto w-full">
           <Breadcrumb pageName={"History"} />
-          <div className="rounded-lg"></div>
+          <div className="overflow-x-auto rounded-lg border border-neutral-200">
+            <table className=" font- w-full min-w-max table-auto  rounded-lg text-center text-base font-medium text-neutral-700">
+              <thead className="bg-neutral-100 text-primary">
+                <tr>
+                  {Table_Head.map((head, index) => (
+                    <th key={index} className="px-2 py-3">
+                      {head}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className="">
+                {history.length === 0 && (
+                  <tr>
+                    <td colSpan="8" className="py-4">
+                      No History Found
+                    </td>
+                  </tr>
+                )}
+                {history.map((appointment, index) => (
+                  <TableRow
+                    key={appointment._id}
+                    index={index}
+                    appointment={appointment}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </>
