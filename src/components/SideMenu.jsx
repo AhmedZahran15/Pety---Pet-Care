85f17e230b3f3374866d5745be055f1a3cdf9cdb
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-19 16:16:50 +0300

    refactor: Update SideMenu component styling

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -9,7 +9,7 @@ export const SideMenu = () => {
   return (
     <menu
       className={
-        "fixed top-0 z-[99] flex h-screen flex-col justify-start gap-8 bg-secondary pl-4 pr-16 pt-16 transition-all duration-200 ease-cubic lg:hidden " +
+        "fixed top-0 z-[99] flex h-screen w-72 flex-col justify-start gap-8 border bg-secondary px-4 pt-16 transition-all duration-200 ease-cubic lg:hidden " +
         (showSideMenu ? "right-0" : "-right-80")
       }
     >
@@ -22,23 +22,23 @@ export const SideMenu = () => {
               className=" h-full scale-[1.3]"
             />
           </picture>
-          <div className="flex flex-col">
-            <span className="text-base font-bold">
+          <div className="flex flex-col items-center">
+            <span className="text-base font-bold text-primary">
               {userData?.firstName} {userData?.lastName}
             </span>
-            <span className="text-sm font-normal text-neutral-100">
+            <span className="text-sm font-medium text-primary opacity-70">
               Service Provider
             </span>
           </div>
         </div>
       ) : null}
-      <ul className="flex flex-col gap-4 text-xl font-bold">
+      <ul className="flex flex-col gap-4 text-xl font-bold text-primary">
         {user ? (
           <li>
             <Link
               to="/dashboard"
               onClick={handleShowSideMenu}
-              className="Medium text-3xl font-medium text-black transition-all duration-300 hover:text-teal-700 sm:hidden"
+              className="nav-link text-3xl font-medium opacity-70 transition-all duration-300 hover:opacity-100 sm:hidden"
             >
               Dashboard
             </Link>
@@ -46,32 +46,32 @@ export const SideMenu = () => {
         ) : null}
         <li
           onClick={handleShowSideMenu}
-          className=" Medium text-3xl font-medium text-black transition-all  duration-300  hover:text-teal-700"
+          className="nav-link relative w-fit text-3xl font-medium opacity-70 transition-all duration-300 hover:opacity-100"
         >
           <Link to="/services?role=vet">Services</Link>
         </li>
         <li
           onClick={handleShowSideMenu}
-          className="Medium gap-2.5 text-3xl font-medium text-black  transition-all  duration-300  hover:text-teal-700"
+          className="nav-link w-fit text-3xl font-medium  opacity-70 transition-all duration-300 hover:opacity-100"
         >
           <Link to="/community">Community</Link>
         </li>
         <li
           onClick={handleShowSideMenu}
-          className=" Medium text-3xl font-medium text-black transition-all  duration-300  hover:text-teal-700"
+          className="nav-link w-fit text-3xl font-medium opacity-70 transition-all duration-300 hover:opacity-100"
         >
           <Link to="/becomeAPety">Become A Pety</Link>
         </li>
 
         <li
           onClick={handleShowSideMenu}
-          className=" Medium text-3xl font-medium text-black transition-all  duration-300  hover:text-teal-700"
+          className="nav-link w-fit text-3xl font-medium opacity-70 transition-all duration-300 hover:opacity-100"
         >
           <Link to="/contact">Contact</Link>
         </li>
         <li
           onClick={handleShowSideMenu}
-          className=" Medium text-3xl font-medium text-black transition-all  duration-300  hover:text-teal-700 "
+          className="nav-link w-fit text-3xl font-medium opacity-70 transition-all duration-300 hover:opacity-100 "
         >
           <Link to="/about">About</Link>
         </li>
@@ -82,13 +82,13 @@ export const SideMenu = () => {
             handleShowSideMenu();
             logoutUser();
           }}
-          className="w-full rounded-3xl bg-[#E3F0F1] px-2 py-1.5 text-center text-xl font-bold text-secondary transition-all duration-100 hover:bg-gray-300  sm:hidden"
+          className="mx-auto mb-4 mt-auto w-60 rounded-3xl bg-secondary px-2 py-1.5 text-center text-xl font-bold text-primary ring-2 ring-primary transition-all duration-100 hover:bg-primary hover:text-secondary  sm:hidden"
         >
           Logout
         </button>
       ) : (
         <Link
-          className="w-full rounded-3xl bg-[#E3F0F1] px-2 py-1.5 text-center text-xl font-bold text-secondary transition-all duration-100 hover:bg-gray-300"
+          className="mx-auto mb-4 mt-auto w-60 rounded-3xl bg-secondary px-2 py-1.5 text-center text-xl font-bold text-primary ring-2 ring-primary transition-all duration-100 hover:bg-primary hover:text-secondary"
           to="auth/login"
         >
           Login
