33a378aea703565e2b52f432ef3f768ee8e32b97
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-20 03:24:43 +0300

    refactor: Update FullButton component styling and class names

--- src/components/FullButton.jsx
+++ src/components/FullButton.jsx
@@ -10,7 +10,7 @@ export const FullButton = ({ text, enabled }) => {
       type="submit"
       disabled={!enabled}
       className={
-        "mt-6 w-1/2 self-center rounded-xl px-4 py-2 text-lg text-white  hover:bg-[#1a8588] disabled:bg-gray-300 " +
+        "hover:bg-primaryDark mt-6 w-1/2 self-center rounded-xl px-4 py-2 text-lg  text-white disabled:bg-gray-300 " +
         (enabled ? "cursor-pointer bg-primary" : "bg-gray-400") +
         (isLoading ? " cursor-not-allowed" : "")
       }
