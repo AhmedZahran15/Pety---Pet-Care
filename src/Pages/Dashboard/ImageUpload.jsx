9ceb0410f37bbbdfaec09d3bf6cb699e155c6fc9
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-20 11:31:24 +0300

    refactor: Update ImageUpload button styles

--- src/Pages/Dashboard/ImageUpload.jsx
+++ src/Pages/Dashboard/ImageUpload.jsx
@@ -15,7 +15,7 @@ function ImageUpload({ imageAsPreview, handleImageChange, photo }) {
           e.preventDefault();
           ref.current.click();
         }}
-        className="cursor-pointer rounded-md bg-emerald-200 px-4 py-2 text-sm font-semibold text-primary shadow-md shadow-neutral-300 hover:bg-emerald-300"
+        className="cursor-pointer rounded-md bg-white px-4 py-2 text-sm font-semibold text-primary shadow-md shadow-neutral-300 ring-1 ring-primary transition-all duration-200 hover:bg-primary hover:text-white hover:ring-primaryDark"
       >
         Choose Image
       </button>
