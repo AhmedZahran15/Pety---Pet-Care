dcd740f485d7e4c124466bbe0dfc7c5700e81efc
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-20 13:24:42 +0300

    refactor: Update PetWorkersHeader component styling and structure

--- src/Pages/Services/PetWorkersHeader.jsx
+++ src/Pages/Services/PetWorkersHeader.jsx
@@ -11,7 +11,7 @@ function PetWorkersHeader({ children }) {
     });
   }, [filterParams]);
   return (
-    <div ref={elementRef} className="-mb-10 flex w-full justify-between">
+    <div ref={elementRef} className="flex h-[54px] w-full justify-between">
       {children}
     </div>
   );
