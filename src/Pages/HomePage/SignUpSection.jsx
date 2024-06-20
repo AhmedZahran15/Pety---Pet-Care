1bb512a4b52d3d4759b0b7f75a91a2fbeef5cd10
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-20 03:08:27 +0300

    refactor: Update SignUpSection component styling and content

--- src/Pages/HomePage/SignUpSection.jsx
+++ src/Pages/HomePage/SignUpSection.jsx
@@ -2,27 +2,25 @@ import { Link } from "react-router-dom";
 
 const SignUpSection = () => {
   return (
-    <div className="mx-8 mt-20 flex items-center justify-center gap-x-16 overflow-hidden duration-300 sm:container sm:mx-auto md:justify-between">
-      <div className="flex h-full flex-col gap-y-6 transition-all duration-300 md:basis-1/2 md:gap-4">
-        <span className="font-fredoka text-3xl font-bold text-neutral-900 sm:text-4xl md:text-5xl">
-          WHO SHOULD USE PETY?
+    <div className="mt-24 flex items-center justify-center gap-x-16 overflow-hidden duration-300 md:justify-between">
+      <div className="hidden bg-[url(/images/homepage/signUpBg.png)] bg-contain bg-no-repeat p-10 pl-20 transition-all duration-300 md:block md:basis-1/2">
+        <img src="/images/homepage/signUpImage.png" alt="Dog" />
+      </div>
+      <div className="container flex h-full flex-col gap-y-6 px-8 transition-all duration-300 md:basis-1/2 md:gap-y-10">
+        <span className=" font-Pacifico text-3xl text-black sm:text-4xl md:text-5xl">
+          Who Should Use Pety?
         </span>
-        <span className="font-['Montserrat'] text-xl font-normal text-neutral-600 md:text-2xl">
+        <span className="font-Montserrat text-xl text-black">
           PETY is the perfect solution for all pet owners who want to provide
           the best care and services for their beloved pets. Whether you&apos;re
-          a busy professional, a pet parent with a hectic schedule, or simply
-          looking for convenience and peace of mind, PETY is designed to meet
-          your needs.
+          a busy professional, or pet parent with a hectic schedule.
         </span>
         <Link
           to="/auth/register"
-          className="w-fit rounded-3xl bg-primary px-4 py-2 text-center font-['Montserrat'] text-2xl font-bold text-white transition-all duration-300 hover:bg-[#015d61]  sm:px-12  sm:py-2.5 "
+          className="hover:bg-primaryDark w-fit rounded-xl bg-primary px-4 py-2 text-center text-2xl font-semibold text-white transition-all duration-300  sm:px-12  sm:py-2.5 "
         >
-          SIGN UP
+          Sign Up
         </Link>
-      </div>
-      <div className="hidden transition-all duration-300 md:block md:basis-1/2">
-        <img src="/images/homepage/Vector 3.png" alt="Dog" />
       </div>
     </div>
   );
