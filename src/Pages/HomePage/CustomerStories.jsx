9566ecfe6cf5f783b97a52a0931934694cd09099
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-20 03:08:13 +0300

    refactor: Update homepage customer stories section

--- src/Pages/HomePage/CustomerStories.jsx
+++ src/Pages/HomePage/CustomerStories.jsx
@@ -1,47 +1,90 @@
-import CustomerStory from "./CustomerStory";
-
-const Stories = [
+const stories = [
   {
-    name: "Mirage",
-    image: "/userImage.png",
+    name: "Ahmed Zahran",
+    image: "/images/homepage/hamster.png",
     title: "The Pet Clinic",
-    text: "I recently used PETY to book a grooming appointment for my dog, and I couldn't be happier with the experience! The platform is user-friendly, making it easy to browse through different grooming salons and choose the one that best suited my needs. The booking process was seamless, and I appreciated the option to select the appointment date and time that worked for me. The grooming salon I chose provided top-notch service, and my dog came back looking and feeling great. I highly recommend PETY to all pet owners looking for hassle-free pet services!",
+    text: "I booked a grooming appointment for my hamster with PETY and couldn't be happier! The platform was easy to use, the booking process was seamless, and the grooming service was top-notch. Highly recommend PETY for hassle-free pet services!",
   },
   {
-    name: "Kareem",
-    image: "/userImage.png",
-    title: "The Pet Clinic",
-    text: "PETY saved the day when I needed to book a last-minute vet appointment for my cat. With PETY's intuitive interface, I was able to quickly find a nearby vet clinic with availability that fit my schedule. The appointment booking process was straightforward, and I received confirmation almost instantly. The vet clinic provided excellent care for my cat, and I was impressed by the professionalism of the staff. PETY truly made the process stress-free, and I'll definitely be using it for all my future pet service needs!",
+    name: "Kareem Azzam",
+    image: "/images/homepage/dog.png",
+    title: "UA clinic",
+    text: "PETY saved the day for a last-minute vet appointment for my cat. The intuitive interface made it easy to find a nearby clinic with availability. The booking process was straightforward, and I got instant confirmation. The clinic's care was excellent.",
   },
   {
-    name: "Amr",
-    image: "/userImage.png",
-    title: "The Pet Clinic",
-    text: "I recently used PETY to find a pet sitter for my dog while I was out of town, and I couldn't have been happier with the experience. PETY's extensive network of pet sitters made it easy to find someone reliable and trustworthy. I appreciated the detailed profiles and reviews of each pet sitter, which helped me make an informed decision. The booking process was quick and efficient, and I felt confident knowing that my dog was in good hands. The pet sitter provided updates and photos throughout the day, which gave me peace of mind. I highly recommend PETY to anyone in need of pet sitting services!",
+    name: "Amr Sherif",
+    image: "/images/homepage/cat.png",
+    title: "PetLife Clinic",
+    text: "I used PETY to find a pet sitter for my dog while I was away, PETY's network of sitters made it easy to find someone reliable. Detailed profiles and reviews helped me choose. Booking was quick, and the updates from the sitter gave me peace of mind.",
   },
 ];
 
 const CustomerStories = () => {
   return (
-    <div className="mx-8 mt-20 flex flex-col items-center justify-center gap-y-8 sm:container sm:mx-auto">
-      <h1 className="font-fredoka text-3xl font-bold text-neutral-800 sm:text-4xl md:text-5xl">
-        CUSTOMER STORIES
+    <div className="container mx-auto mt-20 flex flex-col items-center justify-center gap-y-20 px-8">
+      <h1 className="font-Pacifico self-end text-3xl sm:text-5xl md:text-6xl">
+        Customer Reviews
       </h1>
-      <div className=" flex flex-col gap-6 lg:flex-row">
-        {Stories.map((story, index) => (
-          <CustomerStory
-            key={index}
-            name={story.name}
-            image={story.image}
-            title={story.title}
-            text={story.text}
+      <div className="flex flex-col gap-6 gap-y-16 px-8 sm:px-0 lg:flex-row">
+        <article className="relative flex basis-1/3 flex-col gap-y-4 rounded-2xl bg-white p-4 shadow-md shadow-neutral-300">
+          <img
+            src="/images/homepage/redHeart.png"
+            className="absolute -left-16 -top-24 h-44 w-44"
+            alt="Heart"
           />
-        ))}
+          <div className="-mt-16 h-36 w-36 self-center overflow-clip rounded-full border">
+            <img
+              className="h-40 w-48 scale-[1.4] rounded-full"
+              src={stories[0].image}
+            />
+          </div>
+          <div className="font-Montserrat line-clamp-[12] text-center text-lg italic opacity-70">
+            &quot;{stories[0].text}&quot;
+          </div>
+          <div className="font-Montserrat flex flex-col items-center gap-0 self-center">
+            <span className="text-lg font-semibold">{stories[0].name}</span>
+            <span className="whitespace-pre text-base opacity-70">
+              {stories[0].title}
+            </span>
+          </div>
+        </article>
+        <article className="relative flex basis-1/3 flex-col gap-y-4 rounded-2xl bg-white p-4 shadow-md shadow-neutral-300">
+          <img
+            src="/images/homepage/yellowHearts.png"
+            className="absolute -right-32 -top-12 w-60"
+            alt="Heart"
+          />
+          <div className="-mt-16 h-36 w-36 self-center overflow-clip rounded-full border">
+            <img className="rounded-full" src={stories[1].image} />
+          </div>
+          <div className="font-Montserrat line-clamp-[12] text-center text-lg italic opacity-70">
+            &quot;{stories[1].text}&quot;
+          </div>
+          <div className="font-Montserrat flex flex-col items-center gap-0 self-center">
+            <span className="text-lg font-semibold">{stories[1].name}</span>
+            <span className="whitespace-pre text-base opacity-70">
+              {stories[1].title}
+            </span>
+          </div>
+        </article>
+        <article className="relative flex basis-1/3 flex-col gap-y-4 rounded-2xl bg-white p-4 shadow-md shadow-neutral-300">
+          <div className="-mt-16 h-36 w-36 self-center overflow-clip rounded-full border">
+            <img
+              className="h-40 w-48 scale-[1.4] rounded-full"
+              src={stories[2].image}
+            />
+          </div>
+          <div className="font-Montserrat line-clamp-[12] text-center text-lg italic opacity-70">
+            &quot;{stories[2].text}&quot;
+          </div>
+          <div className="font-Montserrat flex flex-col items-center gap-0 self-center">
+            <span className="text-lg font-semibold">{stories[2].name}</span>
+            <span className="whitespace-pre text-base opacity-70">
+              {stories[2].title}
+            </span>
+          </div>
+        </article>
       </div>
-
-      <button className="w-fit rounded-3xl bg-primary px-4 py-2 text-center font-['Montserrat'] text-2xl font-bold text-white transition-all duration-300 hover:bg-[#015d61]  sm:px-12  sm:py-2.5">
-        Read More
-      </button>
     </div>
   );
 };
