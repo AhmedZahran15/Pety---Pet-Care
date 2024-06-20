d9f27699b193efbe5555aa9cafeaa239aad6cfa9
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-20 13:24:46 +0300

    refactor: Update SearchBar component styling and structure

--- src/Pages/Services/SearchBar.jsx
+++ src/Pages/Services/SearchBar.jsx
@@ -17,9 +17,9 @@ function SearchBar({ name }) {
     );
   }, 300);
   return (
-    <div className="relative w-full shadow-lg shadow-neutral-300">
+    <div className="relative w-full shadow-sm shadow-neutral-200">
       <input
-        className="h-12 w-full rounded-[4px] border-[0.1rem] border-[#999999]  bg-white px-2 pr-16 font-fredoka text-base font-normal focus:outline-none"
+        className="font-fredoka h-[54px] w-full rounded-[4px] border border-neutral-300 bg-white px-2 pr-16 text-lg font-normal focus:border-primary focus:outline-none"
         type="search"
         name="search"
         placeholder="Search"
@@ -30,7 +30,7 @@ function SearchBar({ name }) {
         type="button"
         className=" absolute right-1 top-2 flex cursor-default items-center justify-center border-l-2"
       >
-        <SearchIcon className="mx-1 h-8 w-8 fill-primary py-1" />
+        <SearchIcon className="mx-1 h-9 w-9 fill-primary py-1" />
       </button>
     </div>
   );
