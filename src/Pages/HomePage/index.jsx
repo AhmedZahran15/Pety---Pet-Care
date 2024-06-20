b4502c008a70d19dea1a25a6b2be247abbd3ddb3
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-20 03:08:46 +0300

    refactor: Update homepage layout and components

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,16 +1,16 @@
 import HeroSection from "./HeroSection";
-import FeaturesBar from "../../components/FeaturesBar";
 import Search from "../../components/Search";
 import CustomerStories from "./CustomerStories";
 import FAQ from "./FAQ";
 import SignUpSection from "./SignUpSection";
+import FeaturesSection from "./FeaturesSection";
 function HomePage() {
   return (
-    <div className="m-auto flex flex-col bg-neutral-100">
-      <FeaturesBar imgSrc={"images/homepage/homepageHead.png"} />
-      <Search />
+    <div className="m-auto flex flex-col overflow-x-hidden bg-neutral-100">
       <HeroSection />
       <SignUpSection />
+      <FeaturesSection />
+      <Search />
       <CustomerStories />
       <FAQ />
     </div>
