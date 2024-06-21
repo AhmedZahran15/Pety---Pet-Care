5e8e2e43744fe41b765f2734fbe8594818e1ed2d
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-21 15:13:12 +0300

    refactor: Add carousel screen size to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        carousel: "900px",
+      },
       fontFamily: {
         Pacifico: ["Pacifico", "cursive"],
         Montserrat: ["Montserrat", "sans-serif"],
