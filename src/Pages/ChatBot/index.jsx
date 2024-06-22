e3956bec3088962f6cad5e9964eaaded9120743d
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-23 02:46:46 +0300

    refactor: Remove unused import and reorganize ChatBot page structure

--- src/Pages/ChatBot/index.jsx
+++ src/Pages/ChatBot/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
 import ChatInput from "./ChatInput";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
@@ -11,11 +10,10 @@ const ChatBot = () => {
     <div>
       <div className="flex min-h-screen overflow-hidden lg:ml-72">
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        <ChatInput />
         <div className="relative flex min-h-screen max-w-screen-2xl flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           <main className="relative flex h-full flex-1 p-4 md:p-6 2xl:p-10">
-            <Outlet />
+            <ChatInput />
           </main>
         </div>
       </div>
