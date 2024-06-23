8d0aa59c00170bef7d17dc90eaa3bb15cfa386d4
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-23 16:36:54 +0300

    refactor: Update Tailwind config to remove unused screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      screens: {
-        carousel: "900px",
-      },
       fontFamily: {
         Pacifico: ["Pacifico", "cursive"],
         Montserrat: ["Montserrat", "sans-serif"],
