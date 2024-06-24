53f8528bdd10f3d13f6519b74ea7b9f9ce259557
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-24 19:50:41 +0300

    refactor: Update MyPosts component to include user data and loading state

--- src/Pages/Community/MyPosts.jsx
+++ src/Pages/Community/MyPosts.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, Outlet, useLocation, useSearchParams } from "react-router-dom";
+import { BlockLoader } from "../../components/Loader";
+import Post from "./Post";
 
 function MyPosts() {
   const [searchParams] = useSearchParams();
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const { pathname } = useLocation();
+  const userData = JSON.parse(localStorage.getItem("userData"));
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -19,16 +23,45 @@ function MyPosts() {
           },
         );
         const data = await response.json();
-        console.log(data);
         if (!response.ok) throw new Error(data.message);
         setPosts(data.data);
       } catch (error) {
         console.error(error);
       }
+      setIsLoading(false);
     };
     fetchPosts();
-  }, [searchParams]);
-  return <div></div>;
+  }, [searchParams, pathname]);
+  return (
+    <div className="basis-full pb-4 lg:basis-2/3">
+      <div className="mb-4 flex h-20 w-full items-center gap-x-4 rounded-lg border border-neutral-300 bg-white p-4 shadow-md shadow-neutral-200">
+        <img
+          src={userData?.photo?.url}
+          alt={userData?.firstName}
+          className="h-12 w-12 rounded-full object-cover"
+        />
+        <Link
+          to="new-post"
+          className="w-full rounded-full bg-neutral-100 p-4 transition-all duration-100 hover:bg-neutral-200"
+        >
+          What&apos;s on your mind,&nbsp;
+          {userData?.firstName[0] + userData?.firstName.slice(1)}?
+        </Link>
+      </div>
+      <Outlet />
+      <div className="relative min-h-[150px]">
+        {isLoading ? (
+          <BlockLoader />
+        ) : (
+          <div className="flex flex-col gap-y-4">
+            {posts.map((post) => (
+              <Post key={post._id} post={post} setPosts={setPosts} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default MyPosts;
