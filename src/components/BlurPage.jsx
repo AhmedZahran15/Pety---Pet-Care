55c4d65ae1a3c311a123d54145723cd14486bb7f
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-24 10:50:04 +0300

    refactor: Update BlurPage component to hide on smaller screens

--- src/components/BlurPage.jsx
+++ src/components/BlurPage.jsx
@@ -5,7 +5,7 @@ function BlurPage() {
   return (
     showSideMenu && (
       <div
-        className="fixed left-0 top-0 z-[98] h-full w-full filter backdrop-blur-[3px] transition-all duration-1000 lg:hidden"
+        className="fixed left-0 top-0 z-[98] h-full w-full filter backdrop-blur-[3px] transition-all duration-1000 min-[1120px]:hidden"
         onClick={handleShowSideMenu}
       ></div>
     )
