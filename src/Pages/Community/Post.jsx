768aaa925b4cafb0d42ebcf1120a2d0728a03ab4
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-24 19:26:49 +0300

    refactor: Update Post component to include Bookmark and PostVotes functionality

--- src/Pages/Community/Post.jsx
+++ src/Pages/Community/Post.jsx
@@ -1,27 +1,43 @@
 import moment from "moment";
 import PropTypes from "prop-types";
-function Post({ post }) {
+import Bookmark from "./Bookmark";
+import PostVotes from "./PostVotes";
+function Post({ post, setPosts }) {
+  const userId = JSON.parse(localStorage.getItem("userData"))._id;
   return (
-    <div className="flex flex-col items-start justify-center gap-4 rounded-lg bg-white p-4 shadow-md">
-      <div className="flex w-full items-center justify-between">
-        <div className="flex shrink-0 items-center gap-x-2">
+    <div className="flex flex-col items-start justify-center gap-2 rounded-lg bg-white p-4 font-Montserrat shadow-md">
+      <div className="flex w-full items-center justify-between ">
+        <div className="flex w-full shrink-0 items-center gap-x-2">
           <img
             src={post?.user?.photo?.url ?? "/userImage.png"}
             alt={post.user?.firstName}
-            className="h-12 w-12 rounded-full object-cover"
+            className="h-8 w-8 rounded-full object-cover"
           />
-          <span className="text-lg font-bold">
+          <span className="text-sm font-semibold">
             {post.user?.firstName} {post.user?.lastName}
           </span>
+          <span className="text-sm text-neutral-400">
+            {moment(post.createdAt).fromNow()}
+          </span>
+          <Bookmark
+            postId={post._id}
+            setPosts={setPosts}
+            bookmarked={post.bookmarks.includes(userId)}
+          />
         </div>
-        <span className="text-sm text-neutral-400">
-          {moment(post.createdAt).fromNow()}
-        </span>
       </div>
-      <span className="text-lg font-semibold">{post.title}</span>
-      <div>
-        <p className="mt-4">{post.context}</p>
+      <div className="flex flex-col gap-y-1">
+        <span className="text-lg font-medium">{post.title}</span>
+        {post.tags.map((tag) => (
+          <span
+            key={tag}
+            className="h-6 w-fit rounded-sm border border-neutral-400 p-1 text-xs font-medium text-black"
+          >
+            {tag}
+          </span>
+        ))}
       </div>
+      <p>{post.context}</p>
       <div className="w-full">
         {post.photo && (
           <img
@@ -31,10 +47,14 @@ function Post({ post }) {
           />
         )}
       </div>
+      <div>
+        <PostVotes post={post} setPosts={setPosts} />
+      </div>
     </div>
   );
 }
 Post.propTypes = {
   post: PropTypes.object.isRequired,
+  setPosts: PropTypes.func.isRequired,
 };
 export default Post;
