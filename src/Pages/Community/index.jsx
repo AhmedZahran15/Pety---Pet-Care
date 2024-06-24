4b56efa3863acd21ecf1a6f25c8c836c4d952012
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-24 19:26:39 +0300

    refactor: Add Header component to Community page

--- src/Pages/Community/index.jsx
+++ src/Pages/Community/index.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import HeroSection from "./HeroSection";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
 function Community() {
   return (
     <div className="relative flex flex-col bg-neutral-100">
+      <Header />
       <HeroSection />
       <div className="container mx-auto flex flex-col justify-between px-4 lg:flex-row">
         <Sidebar />
