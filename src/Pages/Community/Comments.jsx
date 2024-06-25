856371e7fc5cdba457c3002ad08c77866af59f54
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-25 07:19:18 +0300

    refactor: Add Comments component to display comments in Community page

---
+++ src/Pages/Community/Comments.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import Comment from "./Comment";
+
+function Comments({ comments }) {
+  return (
+    <div className="flex flex-col divide-y-2 divide-neutral-200 rounded-lg bg-white p-4 font-Montserrat shadow-md">
+      {comments.map((comment) => (
+        <Comment key={comment._id} comment={comment} />
+      ))}
+    </div>
+  );
+}
+Comments.propTypes = {
+  comments: PropTypes.array.isRequired,
+};
+export default Comments;
