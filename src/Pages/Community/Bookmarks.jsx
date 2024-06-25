677f34a338a541eee59e9c44bb3b52d3d9e5cd5d
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-25 03:56:13 +0300

    refactor: Remove unused code and optimize Bookmarks component

--- src/Pages/Community/Bookmarks.jsx
+++ src/Pages/Community/Bookmarks.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet, useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { BlockLoader } from "../../components/Loader";
 import Post from "./Post";
 
@@ -7,8 +7,7 @@ function Bookmarks() {
   const [searchParams] = useSearchParams();
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
-  const userData = JSON.parse(localStorage.getItem("userData"));
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -25,31 +24,15 @@ function Bookmarks() {
         const data = await response.json();
         if (!response.ok) throw new Error(data.message);
         setPosts(data.data);
-        navigate("/community/home");
       } catch (error) {
         console.error(error);
       }
       setIsLoading(false);
     };
     fetchPosts();
-  }, [searchParams, navigate]);
+  }, [searchParams]);
   return (
     <div className="basis-full pb-4 lg:basis-2/3">
-      <div className="mb-4 flex h-20 w-full items-center gap-x-4 rounded-lg border border-neutral-300 bg-white p-4 shadow-md shadow-neutral-200">
-        <img
-          src={userData?.photo?.url}
-          alt={userData?.firstName}
-          className="h-12 w-12 rounded-full object-cover"
-        />
-        <Link
-          to="new-post"
-          className="w-full rounded-full bg-neutral-100 p-4 transition-all duration-100 hover:bg-neutral-200"
-        >
-          What&apos;s on your mind,&nbsp;
-          {userData?.firstName[0] + userData?.firstName.slice(1)}?
-        </Link>
-      </div>
-      <Outlet />
       <div className="relative min-h-[150px]">
         {isLoading ? (
           <BlockLoader />
