8dbf97748dd478b58ec272d550f8e3a9148f0aa7
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-25 13:32:45 +0300

    refactor: Optimize rendering of Comments component by conditionally rendering the container only if there are comments present

--- src/Pages/Community/Comments.jsx
+++ src/Pages/Community/Comments.jsx
@@ -3,13 +3,18 @@ import Comment from "./Comment";
 
 function Comments({ comments }) {
   return (
-    <div className="flex flex-col divide-y-2 divide-neutral-200 rounded-lg bg-white p-4 font-Montserrat shadow-md">
-      {comments.map((comment) => (
-        <Comment key={comment._id} comment={comment} />
-      ))}
-    </div>
+    <>
+      {comments.length > 0 && (
+        <div className="flex flex-col divide-y-2 divide-neutral-200 rounded-lg bg-white p-4 font-Montserrat shadow-md">
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
+
 Comments.propTypes = {
   comments: PropTypes.array.isRequired,
 };
