e07694b1cc3e6936faf639c03a3b82f7b64c2ac9
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-25 06:28:52 +0300

    refactor: Remove unnecessary setPosts prop in Bookmarks component

--- src/Pages/Community/Bookmarks.jsx
+++ src/Pages/Community/Bookmarks.jsx
@@ -39,7 +39,7 @@ function Bookmarks() {
         ) : (
           <div className="flex flex-col gap-y-4">
             {posts.map((post) => (
-              <Post key={post._id} post={post} setPosts={setPosts} />
+              <Post key={post._id} post={post} />
             ))}
           </div>
         )}
