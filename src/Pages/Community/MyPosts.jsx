45b6c459bb213701430d41e2a85bb6cb671ded38
Ahmed Zahran <121820334+AhmedZahran15@users.noreply.github.com>
2024-06-30 18:13:09 +0300

    refactor: Update MyPosts component to improve user display in "What's on your mind" link

--- src/Pages/Community/MyPosts.jsx
+++ src/Pages/Community/MyPosts.jsx
@@ -47,9 +47,9 @@ function MyPosts() {
           className="w-full rounded-full bg-neutral-100 p-4 transition-all duration-100 hover:bg-neutral-200"
         >
           What&apos;s on your mind
-          {userData
-            ? ",&nbsp;" + userData?.firstName[0] + userData?.firstName.slice(1)
-            : ""}
+          <span className="capitalize">
+            {userData ? ", " + userData?.firstName : ""}
+          </span>
           ?
         </Link>
       </div>
